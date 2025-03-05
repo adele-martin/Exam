9525af1d041ea9f1fe54fd916c3282f72dc194d9
Adele Martin <adelejulietmartin@gmail.com>
2025-03-05 16:53:17 +0100

    okok

--- level_2/picoshell.c
+++ level_2/picoshell.c
@@ -4,10 +4,16 @@
 #include <stdio.h>
 #include <sys/wait.h>
 
+
+/*
+The picoshell function is executed by the original (parent) process. It sets up the pipes,
+forks off the child processes to execute the commands, manages the file descriptors,
+and then waits for the children to finish. The parent itself doesn’t run the commands
+directly; it orchestrates their execution by creating child processes.*/
 int    picoshell(char **cmds[])
 {
+	//we start in parent process
 	//I am creating two file descriptors here.
-	//pipefd[0] refers to the read end of the pipe. pipefd[1] refers to the write end of the pipe.
 	int pipe_fds[2];
 	pid_t pid;
 	int i = 0;
@@ -17,30 +23,32 @@ int    picoshell(char **cmds[])
 	{
 		if (cmds[i + 1]) // if there is another cmd after, pipe
 			pipe(pipe_fds);
-		pid = fork();
+		pid = fork(); //a new process is called (copy of parent). they now run independently
 		if (pid == 0)
 		{
 			//you are in the child process
 			if ((prev_fd)) //this is not the first cmd
 			{
+				//this is redirection input
 				dup2(prev_fd, STDIN_FILENO);
 				close(prev_fd);
 			}
 			if (cmds[i + 1])
 			{
-				close(pipe_fds[0]); // Close read end, as the child will write
+				//this is redirection output
+				close(pipe_fds[0]); //Close read end, so it doesn't wait forever
 				dup2(pipe_fds[1], STDOUT_FILENO); //Duplicate the write end
-				close(pipe_fds[1]);
+				close(pipe_fds[1]); //Close write end, the parent does not write to pipe
 			}
 			if (execvp(cmds[i][0], cmds[i]) != 1)
 				exit (1);
 		}
 		else
 		{
-			if (prev_fd) //this is not the first cmd
-			{
+			//we are back in parent process now
+			if (prev_fd) //if this is not the first cmd we close prev_fds
+			//because we will re-assign it below
 				close(prev_fd);
-			}
 			if (cmds[i + 1])
 			{
 				close(pipe_fds[1]);
@@ -51,6 +59,7 @@ int    picoshell(char **cmds[])
 	}
 	while(wait(NULL) > 0)
 		;
+	//The parent and child processes run in parallel, this makes sure both are executed
 	return 0;
 }
 
